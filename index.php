c7489e146bba6283a0bd1af819f5ab9b83689b71
DarioGiovannini <36411527+DarioGiovannini@users.noreply.github.com>
2018-09-26 10:43:51 +0200

    Menu

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script language="JavaScript" type="text/JavaScript" src="scripts/Sorter.js"></script>
+    <script language="JavaScript" type="text/JavaScript" src="scripts/Research.js"></script>
+    <script language="JavaScript" type="text/JavaScript" src="scripts/AJAX.js"></script>
+</head>
+<body style="background-color:green">
+    <div class="centro">
+        <div>
+            <button type="submit" class='btn btn-primary' style="margin:20px"> Giacenze Milano </button>
+            <button type="submit" class='btn btn-primary'  style="margin:20px"> Carichi Rimini </button>
+            <button type="submit" class='btn btn-primary'  style="margin:20px"> Maggior Costo </button>
+        </div>
+    </div>
+
+</body>
+</html>
