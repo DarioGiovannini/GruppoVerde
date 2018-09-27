de5a4f67174d12442033d274f6a223b7d3c2bcd1
chessin2000 <cassinl202@gmail.com>
2018-09-27 14:55:15 +0200

    modifiche grafiche varie

--- index.php
+++ index.php
@@ -11,14 +11,12 @@
     <script language="JavaScript" type="text/JavaScript" src="scripts/Research.js"></script>
     <script language="JavaScript" type="text/JavaScript" src="scripts/AJAX.js"></script>
 </head>
-<body style="background-color:green">
+<body>
     <div align="center">
-        <div>
-            <button type="submit" class='btn btn-primary' style="margin:20px;margin-top:200px" onclick="window.location.href='Crud.php?tabella=giacenzemilano'"> <p style="font-size: 20px"> Giacenze Milano </p> </button> <br>
-            <button type="submit" class='btn btn-primary'  style="margin:20px"  onclick="window.location.href='Crud.php?tabella=carichirimini'"> <p style="font-size: 20px"> Carichi Rimini </p> </button> <br>
-            <button type="submit" class='btn btn-primary'  style="margin:20px"  onclick="window.location.href='Crud.php?tabella=maggiorcosto'"> <p style="font-size: 20px"> Maggior Costo </p> </button> <br>
-        </div>
+        <img src="mediaworld.jpg" style="margin-left:-75px;margin-top:100px;width:1100px;height:140px"> <br>
+        <button type="submit" class='btn btn-danger' style="margin-top:50px" onclick="window.location.href='Crud.php?tabella=giacenzemilano'"> <p style="font-size: 20px"> Giacenze Milano </p> </button> <br>
+        <button type="submit" class='btn btn-danger'  style="margin-top:20px"  onclick="window.location.href='Crud.php?tabella=carichirimini'"> <p style="font-size: 20px"> Carichi Rimini </p> </button> <br>
+        <button type="submit" class='btn btn-danger'  style="margin-top:20px"  onclick="window.location.href='Crud.php?tabella=maggiorcosto'"> <p style="font-size: 20px"> Maggior Costo </p> </button> <br>
     </div>
-
 </body>
 </html>
